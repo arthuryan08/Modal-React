24e1cc6805f75632f5ff0793c21164da63064336
Arthur Yan <arthuryan08@gmail.com>
2021-09-14 16:45:05 -0300

    Modal creating

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const Button = styled.button`
+min-width: 100px;
+padding: 16px 32px;
+border-radius: 4px;
+border: none;
+background: #141414;
+color: #fff;
+font-size: 24px;
+cursor: pointer;
+`
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Button>I'm a modal</Button>
+      </Container>
+    </>
+  );
+}
+
+export default App;
